30957705b747bc0e4f958c7d446ebb05dc106253
peter-miles <80442779+peter-miles@users.noreply.github.com>
2021-04-27 14:05:37 +1000

    added 'internal documentation'

--- program.cs
+++ program.cs
@@ -5,7 +5,7 @@ namespace guess_the_number {
     class program {
         static void Main(string[] args) {
             Random rand = new Random();
-            int correct_num = rand.Next(0, 26);
+            int correct_num = rand.Next(0, 26); // generate random number to guess
             List<int> log = new List<int>();
 
             Console.WriteLine("Hey there! Lets play a little guessing game.");
@@ -13,22 +13,22 @@ namespace guess_the_number {
 
             for (int i = 0; i < 7; i ++) {
                 Console.Write("Enter Guess: ");
-                string input = Console.ReadLine();
-                int guess = int.Parse(input);
-                log.Add(guess);
+                string input = Console.ReadLine(); // get input
+                int guess = int.Parse(input); // convert user input to int
+                log.Add(guess); // add guess to log
 
                 if (guess > correct_num) {
                     Console.WriteLine("Nope, its less than that");
-                    Console.WriteLine("You have {0} guesses left!", 7 - i);
+                    Console.WriteLine("You have {0} guesses left!", 7 - i); // show how many guesses are left
                 } else if (guess < correct_num) {
                     Console.WriteLine("Nope, its greater than that");
                     Console.WriteLine("You have {0} guesses left!", 7 - i);
                 } else if (guess == correct_num) {
                     Console.WriteLine("Damn. You win!");
-                    Console.WriteLine("The number was indeed {0}", correct_num);
-                    Console.WriteLine("You guessed in {0} guesses", (i + 1));
+                    Console.WriteLine("The number was indeed {0}", correct_num); // display correct number
+                    Console.WriteLine("You guessed in {0} guesses", (i + 1)); // show how many guesses it took the user to get
                     Console.WriteLine("Your guess log:");
-                    print_log(log);
+                    print_log(log); // print log
                     return;
                 }
             }
@@ -43,7 +43,7 @@ namespace guess_the_number {
         static public void print_log(List<int> log) {
             Console.Write("[");
             foreach (int i in log) {
-                Console.Write(i + (i == log[log.Count - 1] ? "" : ", "));
+                Console.Write(i + (i == log[log.Count - 1] ? "" : ", ")); // if last number is equal to last number in list dont print a comma
             }
             Console.WriteLine("]");
         }
